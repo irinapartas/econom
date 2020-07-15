eae9baefe5cd1d9e4bd820cdae0939f7e7027dce
Ira <ilovebellsdaya@yandex.ru>
2020-07-15 18:24:09 +0300

    site

---
+++ специальности/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    "use strict";
+
+    initCoursesSlider();
+
+    //    Courses
+
+    function initCoursesSlider() {
+        if ($('.courses_slider').length) {
+            var coursesSlider = $('.courses_slider');
+            coursesSlider.owlCarousel({
+                items: 3,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 8000,
+                nav: false,
+                dots: false,
+                smartSpeed: 1200,
+                margin: 30,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    992: {
+                        items: 3
+                    }
+                }
+            });
+
+            if ($('.courses_slider_prev').length) {
+                var prev = $('.courses_slider_prev');
+                prev.on('click', function() {
+                    coursesSlider.trigger('prev.owl.carousel');
+                });
+            }
+
+            if ($('.courses_slider_next').length) {
+                var next = $('.courses_slider_next');
+                next.on('click', function() {
+                    coursesSlider.trigger('next.owl.carousel');
+                });
+            }
+        }
+    }
+
+});
